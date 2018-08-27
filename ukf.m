d1177a58b78bf3edd031e960cdd49ade0cd37931
yiyang920 <yiyangw@umich.edu>
2018-08-27 17:57:36 -0400

    8.27 update

--- ukf.m
+++ ukf.m
@@ -1,4 +1,4 @@
-function [x_next,P_next,x_dgr,P_dgr,p,K,error1] = ukf(f,h,y,x_hat,P_hat,u,groundtruth,CF_his,P_his,tk1,tk2,config,psum) 
+function [x_next,P_next,x_dgr,P_dgr,p,K,error1] = ukf(f,h,y,x_hat,P_hat,u,groundtruth,CF,CF_his,P_his,tk1,tk2,config,psum) 
 % UKF   Unscented Kalman Filter for nonlinear dynamic systems
 % -------------------------------------------------------------------------
 %
@@ -10,10 +10,11 @@ function [x_next,P_next,x_dgr,P_dgr,p,K,error1] = ukf(f,h,y,x_hat,P_hat,u,ground
 % config.R = R_k
 % x_hat: 
 %       x_hat.x = current state prediction
-%       x_hat.X = current sigma points predictioin
+%       x_hat.X = current sigma points prediction
 % P_hat = current error covariance (predicted)
 % u = input information
 % config.r = chi-square detector parameter
+% CF = Motion model
 % CF_his = history function for DDE of state variable
 % P_his = history function for DDE of covariance matrix
 % groundtruth = groundtruth of the current state
@@ -26,7 +27,9 @@ function [x_next,P_next,x_dgr,P_dgr,p,K,error1] = ukf(f,h,y,x_hat,P_hat,u,ground
 % -------------------------------------------------------------------------
 %
 % Outputs:
-% x_next = next state prediction
+% x_next:
+%        x_next.x = next state prediction
+%        x_next.X = next sigma points prediction
 % P_next = next error covariance (predicted)
 % x_dgr = current state estimate
 % P_dgr = current estimated error covariance
@@ -40,7 +43,7 @@ function [x_next,P_next,x_dgr,P_dgr,p,K,error1] = ukf(f,h,y,x_hat,P_hat,u,ground
 % -------------------------------------------------------------------------
 %
 
-if isa(f,'function_handle') && isa(P_his,'function_handle') && isa(CF_his,'function_handle') && isa(h,'function_handle')
+if isa(f,'function_handle') && isa(CF,'function_handle') && isa(P_his,'function_handle') && isa(CF_his,'function_handle') && isa(h,'function_handle')
     R = config.R;
     Q = config.Q;
     tau = config.tau;
@@ -99,9 +102,74 @@ if isa(f,'function_handle') && isa(P_his,'function_handle') && isa(CF_his,'funct
     else
         OCSVM_r = config.OCSVM_threshold;
         pbar = norm((psum+p.innov)/config.N_ocsvm,1);
-
+        
+        if(pbar <= OCSVM_r(1))
+            [~,score_1d] = predict(config.SVMModel1,p.innov');
+        elseif((pbar <= OCSVM_r(2))&& (pbar > OCSVM_r(1)))
+            [~,score_1d] = predict(config.SVMModel2,p.innov');
+        elseif((pbar <= OCSVM_r(3))&& (pbar > OCSVM_r(2)))
+            [~,score_1d] = predict(config.SVMModel3,p.innov');
+        else
+            [~,score_1d] = predict(config.SVMModel4,p.innov');
+        end
+        
+        if(score_1d < 0)
+            error1 = 1;
+            
+            if(config.use_predict)
+                x_dgr = x;
+                P_dgr = P_hat;
+            else
+                x_dgr = groundtruth;
+                P_dgr = P_hat - K*t*P_hat;
+            end
+            
+        else 
+            x_dgr = x + K* y_tilde;
+            P_dgr = P_hat - K*t*P_hat;
+        end
+            p.score = score_1d;
+    end        
+        
+        y_residual = y - h(x_dgr);                  %residual between the actual measurement and its estimated value
+        p.residual = y_residual;
+        
+        
+        X_dgr = sigmas(x_dgr,P_dgr,c);              %sigma points of current estimation
+        X_next = zeros(size(X_dgr));                %initial predicted sigma points
+        x_next.x = zeros(size(x));
+        
+%         for i = 1:(2*L+1)
+%             
+%             CF_his1 = @(tt) CF_his(tt,X_dgr(:,i));    %store current state estimate to compute the next prediction
+%             sol_X = dde23(f1,tau,CF_his1,[tk1,tk2]);  %solve DDE of state variable
+%             x_next.X(:,i) = sol_X.y(:,end);
+%             x_next.x = x_next.x + Wm(i) * x_next.X(:,i);
+%                        
+%         end
+        
+        CF1 = @(s) CF(s,u); 
+        CF_Sigma = @(X) CF_X(X,CF1);                    %motion model for sigma points matrix
+        
+        CF_X3 = @(t,X,Z) sum(CF_X2(t,X,Z,f1) .* Wm , 2); %****************NEED TO FINISH "W"*************
+        CF_X4 = @(t,X,Z) reshape(X*W*CF_X2(t,X,Z,f1)' + CF_X2(t,X,Z,f1)*W*X' +Q, [],1);
+        
+        CF_X5 = @(t,X,Z) [CF_X3(t,X,Z);CF_X4];
+        
+        f_del1 = @(tt,P,Z) P_d(tt,P,Z,CF_Sigma(X_dgr(:,i),u),Q);
+        P_his_1 = @(tt) reshape(P_his(tt,P_dgr),[],1);  %store current covariance estimate to compute the next prediction
+        sol_P = dde23(f_del1,tau,P_his_1,[tk1,tk2]);    %solve DDE of covariance matrix P
+        P_next = reshape(sol_P.y(:,end),2,2); 
+        
+        
+             
+        
+    
+        p.RMSE = sqrt(mean((groundtruth - x_dgr).^2));  %Root Mean Squared Error
+     
+        
 else
-    error('f, h, CF_his and P_his should be function handles')
+    error('f, h, CF, CF_his and P_his should be function handles')
     return
 end
 end
@@ -144,4 +212,28 @@ function X=sigmas(x,P,c)
 A = c*chol(P)';
 Y = x(:,ones(1,numel(x)));
 X = [x Y+A Y-A]; 
+end
+
+function p = P_d(tt,P,Z,f,Q)
+plag1 = Z(:,1);
+
+a = f(1,1); b = f(1,2); c = f(2,1); d = f(2,2);
+
+p = [a*plag1(1)+b*plag1(3); a*plag1(2)+b*plag1(4);...
+    c*plag1(1)+d*plag1(3); c*plag1(2)+d*plag1(4)] + ...
+    [a*plag1(1)+b*plag1(2); c*plag1(1)+d*plag1(2); ...
+    a*plag1(3) + b*plag1(4); c*plag1(3)+d*plag1(4)] + reshape(Q,[],1);
+end
+
+function cf = CF_X(X,CF)
+% CF is a function handle @(s)!
+C = num2cell(X, 1);                     %Collect the columns into cells
+cf = cellfun(CF, C);          %A 2-by-(2L+1) vector
+end
+
+function cf2 = CF_X2(t,X,Z,f1)
+% f1 is a function handle @(tt,ss,ZZ)!
+C = num2cell(X, 1);                      %Collect the columns into cells
+f11 = @(ss) f1(t,ss,Z);
+cf2 = cellfun(f11, C);
 end
